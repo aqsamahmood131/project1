07c98d6ae46f95ef1d606b4bee11065c99bfb78c
unknown <=sheikhaqsa59>
2021-07-28 09:02:22 -0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>my app</h1>
+      <p>add more work</p>
     </div>
   );
 }
